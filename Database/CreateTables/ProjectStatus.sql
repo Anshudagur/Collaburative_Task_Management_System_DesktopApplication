f09b019d8debb6593a085545a7202346ac19a2ee
Anshu_Dagur <164508557+Anshudagur@users.noreply.github.com>
2026-06-09 16:19:58 +0530

    ProjectStatus Table was created

---
+++ Database/CreateTables/ProjectStatus.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tblProjectStatus(
+    StatusId INT IDENTITY(1,1),
+    StatusName VARCHAR(50) NOT NULL,
+
+    CONSTRAINT PK_tblProjectStatus_StatusId
+        PRIMARY KEY (StatusId),
+    CONSTRAINT UQ_tblProjectStatus_StatusName
+        UNIQUE (StatusName)
+);
