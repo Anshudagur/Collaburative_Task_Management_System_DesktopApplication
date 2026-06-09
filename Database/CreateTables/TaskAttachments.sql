5a7a0f459c2a5dd03f2a67644cc14c23eaa65447
Anshu_Dagur <164508557+Anshudagur@users.noreply.github.com>
2026-06-09 16:21:40 +0530

    TaskAttachments Table was created

---
+++ Database/CreateTables/TaskAttachments.sql
@@ -0,0 +1,15 @@
+CREATE TABLE tblTaskAttachments(
+    TaskId INT NOT NULL,
+    AttachmentId INT NOT NULL,
+
+    CONSTRAINT PK_tblTaskAttachments_TaskId_AttachmentId
+        PRIMARY KEY (TaskId, AttachmentId),
+
+    CONSTRAINT FK_tblTaskAttachments_TaskId_tblTasks
+        FOREIGN KEY (TaskId)
+        REFERENCES tblTasks(TaskId),
+
+    CONSTRAINT FK_tblTaskAttachments_AttachmentId_tblAttachments
+        FOREIGN KEY (AttachmentId)
+        REFERENCES tblAttachments(AttachmentId)
+);
