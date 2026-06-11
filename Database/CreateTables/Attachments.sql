f8e5d04bc9aaf2557fcbb2fac4d2cd08f91b56cd
Anshu_Dagur <164508557+Anshudagur@users.noreply.github.com>
2026-06-11 16:51:26 +0530

    Column datatype was updated to VARBINARY.

--- Database/CreateTables/Attachments.sql
+++ Database/CreateTables/Attachments.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tblAttachments(
-    AttachmentId INT          IDENTITY(1,1),
-    AttachedFile VARCHAR(255) NOT NULL,
+    AttachmentId INT IDENTITY(1,1),
+    AttachedFile VARBINARY(MAX) NOT NULL,
 
     CONSTRAINT PK_tblAttachments_AttachmentId
         PRIMARY KEY (AttachmentId)
